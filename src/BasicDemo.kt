3497851bdf96190b85033086adee2f2a2f64af74
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-26 15:40:35 +0530

    Initial commit

---
+++ src/BasicDemo.kt
@@ -0,0 +1,16 @@
+/**
+ * Created by avinash.ranjan on 26/09/17.
+ */
+
+
+// Main function in kotlin
+fun main(args: Array<String>) {
+    println("Hello world in Kotlin")
+}
+
+
+// Function in kotlin
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
