7ad86e606605929838bb5c66322bc9e72b198751
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-09 10:24:24 +0530

    control flow sample added

---
+++ src/chapter01/ControlFlow.kt
@@ -0,0 +1,24 @@
+package chapter01
+
+val myName = "Avinash Pandey"
+
+fun main(args: Array<String>) {
+    println("This class is sample for Control flow in Kotlin")
+    nameCheck()
+    singleLineControlFlow("Avinash Pandey")
+
+}
+
+fun nameCheck() {
+
+    if (myName.startsWith("A")) {
+        println("$myName is start with A")
+    } else {
+        println("$myName is not starting with A")
+    }
+}
+
+fun singleLineControlFlow(name :String) {
+    var isStartWith = if (myName == name) true else false
+    println("The given name is $isStartWith")
+}
